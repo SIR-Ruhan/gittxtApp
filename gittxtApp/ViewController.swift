1bdbc9e4efd33fff907442c9750a6b14d1d8eb80
Sultan Imtiaz Ruhan SIR <SIR@sir-macbook-pro.local>
2019-01-20 16:22:12 +0600

    Initial Commit

---
+++ gittxtApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittxtApp
+//
+//  Created by Sultan Imtiaz Ruhan on 1/20/19.
+//  Copyright © 2019 NXT lvl garage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
